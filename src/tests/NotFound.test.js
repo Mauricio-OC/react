f760fdb141d4dbbf38b27056d6bd8dd6a9cb0dc2
Mauricio-OC <murisc0@hotmail.com>
2022-09-21 17:40:58 -0300

    req 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
-import Notfound from '../pages/NotFound';
+import { NotFound } from '../pages';
 
-test('testa header do component', () => {
-  renderWithRouter(<Notfound />);
-  const heading = screen.getByRole('heading', { name: /page requested not found/i });
-  expect(heading).toBeInTheDocument();
+it('testa component header', () => {
+  renderWithRouter(<NotFound />);
+  const header = screen.getByText('Page requested not found');
+  expect(header).toBeInTheDocument();
+});
+
+it('testa imagem notfound do pikachu', () => {
+  const texto = 'Pikachu crying because the page requested was not found';
+  const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  renderWithRouter(<NotFound />);
+  const image = screen.queryByAltText(texto);
+  expect(image).toBeInTheDocument();
+  expect(image.src).toEqual(src);
 });
