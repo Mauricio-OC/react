68c41b3a6d35cbbd7036514741498b1096da2320
Mauricio-OC <murisc0@hotmail.com>
2022-09-20 22:58:21 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Notfound from '../pages/NotFound';
+
+test('testa header do component', () => {
+  renderWithRouter(<Notfound />);
+  const heading = screen.getByRole('heading', { name: /page requested not found/i });
+  expect(heading).toBeInTheDocument();
+});
