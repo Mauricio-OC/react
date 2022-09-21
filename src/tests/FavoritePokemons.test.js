768ddcc6b97afbdcb96e07ed80fde3eff25c0668
Mauricio-OC <murisc0@hotmail.com>
2022-09-20 22:10:14 -0300

    req 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const notFound = screen.getByText(/no favorite pokemon found/i);
+  expect(notFound).toBeInTheDocument();
+});
